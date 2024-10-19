0a3bd2e136c1969f0bf72ad65a5e369087e4c7a8
ehvidalp <ehvidalp@gmail.com>
2024-10-18 20:58:34 -0600

    refactor: Update Dashboard component and add Tailwind CSS styles

--- src/features/dashboard/pages/Dashboard.tsx
+++ src/features/dashboard/pages/Dashboard.tsx
@@ -1,7 +1,11 @@
 
 
 const Dashboard = () => {
-  return <div>Dashboard</div>
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+    <h1 className="text-4xl font-bold text-blue-600">¡Hola, Tailwind CSS!</h1>
+  </div>
+  )
 }
 
 export default Dashboard
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,17 +1,15 @@
-// src/routes/AppRoutes.tsx
-import React, { Suspense } from 'react';
+import { Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { DashboardRoutes } from '../features/dashboard';
 import { ItemRoutes } from '../features/Item';
 
-const AppRoutes: React.FC = () => {
+const AppRoutes = () => {
   return (
     <Router>
       <Suspense fallback={<div>Cargando...</div>}>
         <Routes>
           <Route path="/*" element={<DashboardRoutes />} />
           <Route path="/pokemon/*" element={<ItemRoutes />} />
-          {/* Redirigir al Dashboard si la ruta no existe */}
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Suspense>
